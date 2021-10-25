8c1444da98d125d591055457996a7b76cdc827c3
denis garifyanov <daneegar@gmail.com>
2021-10-25 22:41:20 +0300

    Проект #1/Step-3
    **Содержание:**
    Задачи
    Подключите в зависимости библиотеку prompt (командой poetry add prompt)
    
    Поприветствуйте пользователя в игре. Спросите у пользователя его имя и поприветствуйте его по имени:
    
    $ brain-games
    Welcome to the Brain Games!
    May I have your name? John
    Hello, John!
    После чего программа завершается.
    
    Опишите эту логику в функции welcome_user файла brain_games/cli.py и поместите вызов этой функции в скрипт brain_games/scripts/brain_games.py.
    
    Выполните сборку новой версии пакета.
    
    Проведите отладку публикации.

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,14 @@
 # file <brain_games.py>
-#!/usr/bin/env python
+#!/usr/bin/env python3
+from . import cli
+from .cli import welcome_user
 
 def greet():
     print('Welcome to the Brain Games!')
 
 def main():
     greet()
+    welcome_user()
 
 if __name__ == '__main__':
     main()
